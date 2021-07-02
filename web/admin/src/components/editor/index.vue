643b52f23c6d6baf9259481083efd5d8c5c81190
suixinio <suixinio@163.com>
2021-07-02 14:28:09 +0800

    feat:vditor

--- web/admin/src/components/editor/index.vue
+++ web/admin/src/components/editor/index.vue
@@ -1,42 +1,113 @@
 <template>
   <div>
-    <Editor :init='init' v-model='content'></Editor>
+    <!--    <Editor :init='init' v-model='content'></Editor>-->
+
+    <div id='vditor' class='vditor' />
   </div>
 </template>
 
 <script>
-import Editor from '@tinymce/tinymce-vue'
+// import Editor from '@tinymce/tinymce-vue'
 // eslint-disable-next-line no-unused-vars
-import tinymce from './tinymce.min'
-import './icons/default/icons.min'
-import './themes/silver/theme.min'
-import './langs/zh_CN'
+// import tinymce from './tinymce.min'
+// import './icons/default/icons.min'
+// import './themes/silver/theme.min'
+// import './langs/zh_CN'
+
+import Vditor from 'vditor'
+import 'vditor/dist/index.css'
 
 export default {
-  components: { Editor },
+  // components: { Editor },
+
   props: {
     value: {
       type: String,
       default: ''
     }
   },
-
   data () {
     return {
-      init: {
-        language: 'zh_CN'
+      contentEditor: ''
+    }
+  },
+  created () {
+    this.setDefaultText()
+    console.log(this.value)
+  },
+  mounted () {
+    const that = this
+    const options = {
+      typewriterMode: true,
+      after () {
+        console.log('渲染完成')
+        that.contentEditor.setValue(that.value)
       },
-      content: this.value
+      ctrlEnter (md) {
+        console.log('用户按下了 Ctrl+Enter，Markdown 内容为：\n' + md)
+        that.content = md
+        that.$emit('input', md)
+      },
+      input (md) {
+        console.log('用户进行了输入，Markdown 内容为：\n' + md)
+        that.content = md
+        that.$emit('input', md)
+      },
+      blur (md) {
+        console.log('用户离开了编辑器，Markdown 内容为：\n' + md)
+        that.content = md
+        that.$emit('input', md)
+      },
+      select (md) {
+        console.log('用户选中了一段文字，内容为：\n' + md)
+        // this.content = md
+      },
+      focus (md) {
+        console.log('用户选中了编辑器，Markdown 内容为：\n' + md)
+        that.content = md
+        that.$emit('input', md)
+      },
+      esc (md) {
+        console.log('用户按下了 ESC，Markdown 内容为：\n' + md)
+        that.content = md
+        that.$emit('input', md)
+      },
+      width: this.isMobile ? '100%' : '80%',
+      height: '0',
+      tab: '\t',
+      counter: '999999',
+      mode: 'sv',
+      preview: {
+        delay: 100,
+        show: !this.isMobile
+      },
+      outline: true
+    }
+    this.contentEditor = new Vditor('vditor', options)
+    // this.contentEditor.setValue('123123')
+    this.contentEditor.clearCache()
+  },
+  methods: {
+    update (str) {
+      this.contentEditor.setValue('123123')
+    },
+    setDefaultText () {
+      const savedMdContent = localStorage.getItem('vditorvditor') || ''
+      if (!savedMdContent.trim()) {
+        localStorage.setItem('vditorvditor', '12312312313')
+      }
     }
   },
   watch: {
     value (newV) {
+      console.log('value' + newV)
       this.content = newV
+      this.contentEditor.setValue(newV)
     },
     content (newV) {
+      console.log('content:' + newV)
       this.$emit('input', newV)
     }
-
   }
 }
 </script>
